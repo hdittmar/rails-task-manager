4237f939b06a193ea0e95c723269d6699a96a1e7
Henrik Dittmar <henrik.dittmar@gmx.de>
2018-02-13 16:19:09 +0100

    full working without styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :tasks
+
+  # #index
+  # get 'tasks', to: 'tasks#index'
+
+
+  # #new
+  # get 'tasks/new', to: 'tasks#new'
+  # post 'tasks', to: 'tasks#create'
+
+  # #show
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # #update
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  # patch 'tasks/:id', to: 'tasks#update'
+
+  # #destroy
+  # get 'tasks/:id/delete', to: 'tasks#destroy', as: :destroy
 end
